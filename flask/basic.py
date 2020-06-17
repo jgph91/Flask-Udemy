cd11100b262162a3b9f3a2a92379681ee0c65915
jgph91 <jgph91@gmail.com>
2020-06-17 20:17:15 +0200

    templates

---
+++ flask/basic.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    puppies = ['Fluffy','Rufus','Milu','Yako']
+    
+    return render_template('basic.html',puppies=puppies)
+
+if __name__ == '__main__':
+    app.run(debug=True)
